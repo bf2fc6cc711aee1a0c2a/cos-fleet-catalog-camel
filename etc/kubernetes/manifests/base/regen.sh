51dcbcca148f706908e18b7d03ad9fea43dc00f9
Luca Burgazzoli <lburgazzoli@gmail.com>
2022-05-14 18:35:58 +0200

    ci: fix regen script

--- etc/kubernetes/manifests/base/regen.sh
+++ etc/kubernetes/manifests/base/regen.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-DIR=$(dirname "${BASH_SOURCE[0]}")
-CONNECTORS_DIR="${DIR}/connectors"
+CONNECTORS_DIR="connectors"
 
-cat /dev/null > "${DIR}/kustomization.yaml"
+cat /dev/null > "kustomization.yaml"
 
 for D in "${CONNECTORS_DIR}"/*; do
   CM_NAME=$(basename "${D}")
