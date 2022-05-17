0abddd2747d131042206f4680105c7bcba667a6a
Andrea Tarocchi <andrea.tarocchi@gmail.com>
2022-05-17 14:57:49 +0200

    Added customizer to reoove aws_use_default_credentials_provider property from aws connectors

--- cos-fleet-catalog-connectors/aws/etc/customizers/mandatory_accesskey_secretkey_properties.groovy
+++ cos-fleet-catalog-connectors/aws/etc/customizers/mandatory_accesskey_secretkey_properties.groovy
@@ -2,4 +2,6 @@
 def requiredProp = schema.requiredAt('/required')
 
 requiredProp.add('aws_access_key')
+log.info("Added aws_access_key to required properties")
 requiredProp.add('aws_secret_key')
+log.info("Added aws_secret_key to required properties")
