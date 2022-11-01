30b390ccdef3f918d7f7b07e0b27db78f090b280
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2022-11-01 09:28:14 -0300

    fix MGDCTRS-1548: Azure Storage Blob changefeed source connector the connector produce one record per event the connector produced data are in json format the connector add additional info as record headers

---
+++ cos-fleet-catalog-connectors-it/azure/storage-blob-changefeed-it/src/test/groovy/org/bf2/cos/connector/camel/it/azure/storage/blob/AzureStorageBlobChangefeedConnectorContainerIT.groovy
@@ -0,0 +1,34 @@
+package org.bf2.cos.connector.camel.it.azure.storage.blob
+
+import groovy.util.logging.Slf4j
+import org.bf2.cos.connector.camel.it.support.ConnectorContainer
+import org.bf2.cos.connector.camel.it.support.SimpleConnectorSpec
+
+@Slf4j
+class AzureStorageBlobChangefeedConnectorContainerIT extends SimpleConnectorSpec {
+
+    def "container image exposes health and metrics"(String definition) {
+        setup:
+            def cnt = ConnectorContainer.forDefinition(definition).build()
+            cnt.start()
+        when:
+            def health = cnt.request.get('/q/health')
+            def metrics = cnt.request.get("/q/metrics")
+        then:
+            health.statusCode == 200
+            metrics.statusCode == 200
+
+            with (health.as(Map.class)) {
+                status == 'UP'
+                checks.find {
+                    it.name == 'context' && it.status == 'UP'
+                }
+            }
+        cleanup:
+            closeQuietly(cnt)
+        where:
+            definition << [
+                'azure_storage_blob_changefeed_source_0.1.json'
+            ]
+    }
+}
