3dfcf5a4e7f085ac0a393040344b7898421172d3
Rinaldo Pitzer Júnior <16694899+rinaldodev@users.noreply.github.com>
2022-11-08 10:57:11 -0300

    fix: google functions tests

--- cos-fleet-catalog-connectors-it/gcp/google-functions-it/src/test/groovy/org/bf2/cos/connector/camel/it/ConnectorIT.groovy
+++ cos-fleet-catalog-connectors-it/gcp/google-functions-it/src/test/groovy/org/bf2/cos/connector/camel/it/ConnectorIT.groovy
@@ -2,6 +2,7 @@ package org.bf2.cos.connector.camel.it
 
 import groovy.util.logging.Slf4j
 import org.bf2.cos.connector.camel.it.support.KafkaConnectorSpec
+import org.bf2.cos.connector.camel.it.support.TestUtils
 import spock.lang.IgnoreIf
 
 import java.net.http.HttpClient
@@ -15,21 +16,28 @@ import java.util.concurrent.TimeUnit
     !hasEnv('GCP_FUNCTIONS_PROJECT_ID'            ) ||
     !hasEnv('GCP_FUNCTIONS_FUNCTION_NAME'         ) ||
     !hasEnv('GCP_FUNCTIONS_REGION'                ) ||
-    !hasEnv('GCP_FUNCTIONS_SERVICE_ACCOUNT_KEY'   ) ||
-    !hasEnv('GCP_FUNCTIONS_WEBHOOK_ID'   )
+    !hasEnv('GCP_FUNCTIONS_SERVICE_ACCOUNT_KEY'   )
 })
 @Slf4j
 class ConnectorIT extends KafkaConnectorSpec {
+
+    private static final Duration HTTP_TIMEOUT = Duration.of(5, ChronoUnit.SECONDS)
+    private static final HttpClient HTTP_CLIENT = HttpClient.newBuilder()
+                                                    .connectTimeout(HTTP_TIMEOUT)
+                                                    .build();
+
     @Override
     def setupSpec() {
     }
 
     def "google functions sink"() {
         setup:
+            def webhookId = createWebhookToken()
+            assert webhookId
+
             def kafkaTopic = topic()
             def kafkaGroup = UUID.randomUUID().toString()
             def message = UUID.randomUUID().toString()
-            def webhookId = System.getenv('GCP_FUNCTIONS_WEBHOOK_ID')
             def payload =   """
                                     {"url":"https://webhook.site/${webhookId}", "message":"${message}"}
                                     """
@@ -53,17 +61,18 @@ class ConnectorIT extends KafkaConnectorSpec {
             records.first().value() == payload
 
             def queryURI = URI.create("http://webhook.site/token/${webhookId}/requests?query=${message}")
-            def timeout = Duration.of(5, ChronoUnit.SECONDS)
-            def request = HttpRequest.newBuilder(queryURI).GET().timeout(timeout).build()
+            def request = HttpRequest.newBuilder(queryURI).GET().timeout(HTTP_TIMEOUT).build()
 
-            await(16, TimeUnit.SECONDS) {
+            await(21, TimeUnit.SECONDS) {
                 try {
-                    return HttpClient.newBuilder()
-                            .connectTimeout(timeout)
-                            .build()
-                            .send(request, HttpResponse.BodyHandlers.ofString())
-                            .body()
-                            .contains(message);
+                    def response = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString())
+
+                    def body = response.body()
+                    if (response.statusCode() / 100 != 2) {
+                        log.info("Bad status code when trying to check webhook. Response: {}", body)
+                    }
+
+                    return body.contains(message);
                 } catch (Exception e) {
                     log.info("Exception while checking webhook.", e)
                     return false;
@@ -71,6 +80,23 @@ class ConnectorIT extends KafkaConnectorSpec {
             }
         cleanup:
             closeQuietly(cnt)
+    }
+
+    static String createWebhookToken() {
+        def postURI = URI.create("http://webhook.site/token/")
+        def request = HttpRequest.newBuilder(postURI)
+                .POST(HttpRequest.BodyPublishers.noBody())
+                .timeout(HTTP_TIMEOUT)
+                .build()
+
+        def body = ""
+        try {
+            body = HTTP_CLIENT.send(request, HttpResponse.BodyHandlers.ofString()).body()
+            return TestUtils.SLURPER.parseText(body)["uuid"];
+        } catch (Exception e) {
+            log.info("Exception while creating webhook. Body: {}", body, e)
+            throw e;
+        }
     }
 
 }
