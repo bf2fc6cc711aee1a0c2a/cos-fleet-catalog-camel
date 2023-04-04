6c77c3df465aaae14b4cf0a29f671d48b7c43334
Luca Burgazzoli <lburgazzoli@gmail.com>
2023-04-04 21:50:29 +0200

    Temporary remove camel-quarkus-jq

--- cos-fleet-catalog-connectors-it/sql/postgresql-it/src/test/groovy/org/bf2/cos/connector/camel/it/ConnectorIT.groovy
+++ cos-fleet-catalog-connectors-it/sql/postgresql-it/src/test/groovy/org/bf2/cos/connector/camel/it/ConnectorIT.groovy
@@ -54,6 +54,51 @@ class ConnectorIT extends KafkaConnectorSpec {
                 db_password: ${db.password}
                 db_query: 'INSERT INTO accounts (username,city) VALUES (:#username,:#city)'
                 db_database_name: ${db.databaseName}
+            """)
+
+            cnt.start()
+        when:
+            kafka.send(topic, payload)
+        then:
+            def records = kafka.poll(group, topic)
+            records.size() == 1
+            records.first().value() == payload
+
+            await(30, TimeUnit.SECONDS) {
+                return sql.rows("""SELECT * FROM accounts WHERE username='foo';""").size() == 1
+            }
+
+        cleanup:
+            closeQuietly(sql)
+            closeQuietly(cnt)
+    }
+
+    @spock.lang.Ignore("temporary disabled for CPaaS")
+    def "postgresql sink with transform"() {
+        setup:
+            def sql = Sql.newInstance(db.jdbcUrl,  db.username, db.password, db.driverClassName)
+            def payload = '''{ "username":"foo", "city":"Rome" }'''
+
+            sql.execute("""
+                CREATE TABLE accounts (
+                   username VARCHAR(50) UNIQUE NOT NULL,
+                   city VARCHAR(50)
+                );
+            """)
+
+            def topic = topic()
+            def group = UUID.randomUUID().toString()
+
+            def cnt = connectorContainer('postgresql_sink_0.1.json', """
+                kafka_topic : ${topic}
+                kafka_bootstrap_servers: ${kafka.outsideBootstrapServers}
+                kafka_consumer_group: ${UUID.randomUUID()}
+                db_server_name: ${CONTAINER_NAME}
+                db_server_port: ${PostgreSQLContainer.POSTGRESQL_PORT}
+                db_username: ${db.username}
+                db_password: ${db.password}
+                db_query: 'INSERT INTO accounts (username,city) VALUES (:#username,:#city)'
+                db_database_name: ${db.databaseName}
                 processors:
                 - transform:
                     jq: '.username = "oscerd"'
